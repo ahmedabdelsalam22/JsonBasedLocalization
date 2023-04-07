b60a8e308e4774a4eb75966d9576e3177716d3d3
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-04-07 21:35:48 +0200

    create GetString method in JsonStringLocalizer

--- JsonBasedLocalization.Web/JsonStringLocalizer.cs
+++ JsonBasedLocalization.Web/JsonStringLocalizer.cs
@@ -5,6 +5,10 @@ namespace JsonBasedLocalization.Web
 {
     public class JsonStringLocalizer : IStringLocalizer
     {
+
+        private readonly JsonSerializer _serializer = new JsonSerializer();
+
+
         public LocalizedString this[string name] => throw new NotImplementedException();
 
         public LocalizedString this[string name, params object[] arguments] => throw new NotImplementedException();
@@ -24,6 +28,18 @@ namespace JsonBasedLocalization.Web
             using FileStream stream = new FileStream(filePath,FileMode.Open , FileAccess.Read , FileShare.Read);
             StreamReader streamReader = new StreamReader(stream);
             using JsonTextReader reader = new JsonTextReader(streamReader);
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.PropertyName && reader.Value as string == propertyName)
+                {
+                    reader.Read();
+                    return _serializer.Deserialize<string>(reader);
+
+                }
+            }
+
+            return string.Empty;
         }
     }
 }

8725e827f6bf77503a561b6a9d80539550e7999d
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-04-08 14:31:50 +0200

    add TestController and view

---
+++ JsonBasedLocalization.Web/ViewModels/CreateTestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace JsonBasedLocalization.Web.ViewModels
+{
+    public class CreateTestViewModel
+    {
+        [Display(Name = "name"), Required(ErrorMessage = "required")]
+        public string Name { get; set; }
+    }
+}
